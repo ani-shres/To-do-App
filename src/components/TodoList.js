ae5e726a62922e5ca290bb6aa7457c48b62381d7
ani_shres <anisha.shrestha@hotmail.com>
2022-01-04 15:17:04 +1100

    removed custom animation function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,10 +5,7 @@ import Item from "./Item";
 import FlipMove from "react-flip-move";
 
 const TodoList = forwardRef(({ items, setItems, ref }) => {
-  const customLeaveAnimation = {
-    from: { transform: "scale(1, 1)" },
-    to: { transform: "scale(0.5, 1) translateY(-20px)" },
-  };
+
   return (
     <div >
       <h4> List of Todos</h4>
